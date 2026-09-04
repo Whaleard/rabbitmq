4d93cd2a4986aa6fddeaf864a5dd7bd564492190
chengmengd <chengmengd@isoftstone.com>
2026-09-04 11:29:34 +0800

    消息队列不公平分发

--- src/main/java/com/example/rabbitmq/three/Worker02.java
+++ src/main/java/com/example/rabbitmq/three/Worker02.java
@@ -33,6 +33,10 @@ public class Worker02 {
             }
         };
 
+        // 设置不公平分发
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
+
         // 采用手动应答
         boolean autoAck = false;
         channel.basicConsume(TASK_QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
