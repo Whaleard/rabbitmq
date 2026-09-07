6822af0a00c4518ea1c714a8e9ddf2343609d96c
chengmengd <chengmengd@isoftstone.com>
2026-09-07 18:06:35 +0800

    死信队列处理正常情况下队列达到最大长度

---
+++ src/main/java/com/example/rabbitmq/eight/MaxLengthProductor.java
@@ -0,0 +1,26 @@
+package com.example.rabbitmq.eight;
+
+import com.example.rabbitmq.util.RabbitMqUtil;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+
+/**
+ * 生产者（队列达到最大长度案例）
+ */
+public class MaxLengthProductor {
+
+    /**
+     * 正常情况交换机名称
+     */
+    public static final String NORMAL_EXCHANGE = "normal_exchange";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtil.getChannel();
+
+        for (int i = 0; i < 10; i++) {
+            String message = "info" + i;
+            channel.basicPublish(NORMAL_EXCHANGE, "normal", null, message.getBytes());
+        }
+
+    }
+}
